55f1e886a299a5d1955c6188dbba25b4c41d7a0f
Uzair <programmeruzair@gmail.com>
2026-02-03 12:29:35 +0530

    Update App component to display "Portfolio Coming Soon!" message with links to GitHub and LinkedIn

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,12 @@ const App = () => {
   return (
     <>
       <Layout>
-        <div className='bg-bg h-screen w-full'>
-          hello world
+        <div className='bg-bg h-screen w-full flex justify-center items-center flex-col'>
+          <h1 className='text-white text-2xl md:text-5xl font-bold'>Portfolio Comming Soon!</h1>
+          <div className='mt-10 flex gap-24 text-xl md:text-3xl font-bold'>  
+            <a href="https://github.com/UzairProg" target='_blank' className='text-white underline mt-4 block text-center'>Github</a>
+            <a href="https://www.linkedin.com/in/uzair-md" target='_blank' className='text-white underline mt-4 block text-center'>LinkedIn</a>
+          </div>
         </div>
       </Layout>
     </>
